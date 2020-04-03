51245755803af049afe7d22663512a1ae9c0d6f4
Senad <senad.sali3@gmail.com>
2020-04-03 15:47:53 +0200

    Updates

---
+++ precache-manifest.bbe5fb46b6acb2017a9714e3a6e72d73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0da68297893ec353f187fc448d19983a",
+    "url": "/react-flux/index.html"
+  },
+  {
+    "revision": "448c3b60673bf2871ac6",
+    "url": "/react-flux/static/css/2.3ea52fea.chunk.css"
+  },
+  {
+    "revision": "448c3b60673bf2871ac6",
+    "url": "/react-flux/static/js/2.e86478c2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-flux/static/js/2.e86478c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08e77ccb51e5378aca19",
+    "url": "/react-flux/static/js/main.fc0e4e41.chunk.js"
+  },
+  {
+    "revision": "ac24f6a5c1de69e577ce",
+    "url": "/react-flux/static/js/runtime-main.df8cd716.js"
+  }
+]);
